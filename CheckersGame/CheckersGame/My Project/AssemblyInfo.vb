84d104ed256573a602541d6c6b5d8a13f6cfa5b8
Natalia2000Bush <62169930+Natalia2000Bush@users.noreply.github.com>
2020-03-14 11:46:24 +0300

    Initial commit

---
+++ CheckersGame/CheckersGame/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("CheckersGame")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CheckersGame")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("931ec3f0-f261-4a51-8010-588e1f9bfb71")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номера сборки и редакции по умолчанию 
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
